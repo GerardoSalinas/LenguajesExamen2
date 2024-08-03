39f78973989f245311c8fc1b939dd9e29baa8d3a
gasalinas <gerardoaplicano16@gmail.com>
2024-08-02 19:56:56 -0600

    Fixes bug

--- src/main/java/unah/lenguajes/hn/examen2/services/PrestamoServicio.java
+++ src/main/java/unah/lenguajes/hn/examen2/services/PrestamoServicio.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import unah.lenguajes.hn.examen2.models.Cliente;
+import unah.lenguajes.hn.examen2.models.Cuota;
 import unah.lenguajes.hn.examen2.models.Prestamo;
 import unah.lenguajes.hn.examen2.repositories.ClienteRepositorio;
+import unah.lenguajes.hn.examen2.repositories.CuotaRepositorio;
 import unah.lenguajes.hn.examen2.repositories.PrestamoRepositorio;
 
 @Service
@@ -17,6 +19,9 @@ public class PrestamoServicio {
 
     @Autowired
     private ClienteRepositorio clienteRepositorio;
+
+    @Autowired
+    private CuotaRepositorio cuotaRepositorio;
 
     public Prestamo buscarPrestamo(long codigoPrestamo){
         if (this.prestamoRepositorio.existsByCodigoPrestamo(codigoPrestamo)){
@@ -36,6 +41,25 @@ public class PrestamoServicio {
                 double i = 0.09/12;
                 double nvaCouta = monto*i / 1 - Math.pow((1+i),plazo*12);
                 prestamo.setCuota(nvaCouta);
+
+                //genera cuotas
+                Cuota cuotaAnterior = new Cuota();
+                    cuotaAnterior.setMes(0);
+                    cuotaAnterior.setCapital(0);
+                    cuotaAnterior.setInteres(0);
+                    cuotaAnterior.setSaldo(prestamo.getMonto());
+                    
+                    for (int j = 1; j<plazo*12; j++){
+                        Cuota siguienteCuota = new Cuota();
+                        siguienteCuota.setMes(j);
+                        siguienteCuota.setInteres(cuotaAnterior.getSaldo()*i);
+                        siguienteCuota.setCapital(nvaCouta-siguienteCuota.getInteres());
+                        siguienteCuota.setSaldo(cuotaAnterior.getSaldo()-siguienteCuota.getCapital());
+                        siguienteCuota.setPrestamo(prestamo);
+                        this.cuotaRepositorio.save(siguienteCuota);
+                        cuotaAnterior = siguienteCuota;
+                    }
+
                 return this.prestamoRepositorio.save(prestamo);
             }
         }
